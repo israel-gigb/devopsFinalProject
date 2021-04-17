ffd616b9ec5c7048f16cbfb489c036c7403670fe
Israel Garza <israel.gigb@gmail.com>
2021-04-16 21:27:36 -0500

    Update config.php

--- website/config.php
+++ website/config.php
@@ -2,7 +2,7 @@
 
 /**
  * Used to store website configuration information.
- * abcdefghijklmnopqrstuvwxyzabcdefghijkli
+ * abcdefghijklmnopqrstuvwxyzabcdefghijklim
  * @var string
  */
 function config($key = '')
